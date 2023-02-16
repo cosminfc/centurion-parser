fe43083ec061fcde327924c09a79a95f1a81f113
cosminfc <cozmacosmin1992@gmail.com>
2023-02-16 21:05:23 +0100

    Jest testing tool setup

---
+++ test/index.test.ts
@@ -0,0 +1,7 @@
+import {greeting} from "../src";
+
+describe("test greeting function", () => {
+    it("should return \"Hello World\"", () => {
+        expect(greeting()).toBe("Hello World");
+    });
+});
